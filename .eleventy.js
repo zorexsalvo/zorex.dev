83f41b3a9872ec1f9206a66e7c3140debd0322fe
Zorex Salvo <zorexsalvo@gmail.com>
2026-07-23 21:25:04 +0800

    fix: add .nojekyll to prevent GitHub Pages from using Jekyll

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
 module.exports = function(eleventyConfig) {
-  // Passthrough for CSS and favicon files
+  // Passthrough for CSS, favicon, and nojekyll files
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/favicon.svg");
+  eleventyConfig.addPassthroughCopy("src/.nojekyll");
 
   return {
     dir: {
